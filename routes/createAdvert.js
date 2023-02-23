2bf94599b7b8574255bc5c275cb5dc50003d5757
Reyes Morales Dominguez <reyesmoralesdominguez@gmail.com>
2023-02-23 21:11:22 +0000

    Añadir error 400

--- routes/createAdvert.js
+++ routes/createAdvert.js
@@ -4,20 +4,20 @@ var Anuncio = require('../models/Anuncio');
 
 router.post('/', async (req, res, next) => {
     try {
-  
-      const anuncioData = req.body;
-  
-      // creamos una instancia de Agente en memoria
-      const anuncio = new Anuncio (anuncioData);
-  
-      // la persistimos en la BD
-      const anuncioGuardado = await anuncio.save();
-  
-      res.json({ result: anuncioGuardado });
-  
+
+        const anuncioData = req.body;
+
+        // creamos una instancia de Agente en memoria
+        const anuncio = new Anuncio(anuncioData);
+
+        // la persistimos en la BD
+        const anuncioGuardado = await anuncio.save();
+
+        res.json({ result: anuncioGuardado });
+
     } catch (error) {
-      next(error);
+        res.status(400).json({ error: 'Falta algún parámetro"'});
     }
-  });
+});
 
 module.exports = router;
