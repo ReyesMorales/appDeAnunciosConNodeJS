e14a46da619e070a21b22446305029a78b10d721
Reyes Morales Dominguez <reyesmoralesdominguez@gmail.com>
2023-02-23 20:43:44 +0000

    Añadir logica para eliminar anuncios

--- routes/deleteAdvert.js
+++ routes/deleteAdvert.js
@@ -1,8 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var Anuncio = require('../models/Anuncio');
 
-router.delete('/', function(req, res, next) {
-    console.log('888advert')
-});
+router.delete('/:id', async (req, res, next) => {
+    try {
+  
+      const id = req.params.id;
+  
+      await Anuncio.deleteOne({ _id: id });
+  
+      res.json();
+  
+    } catch (error) {
+      next(error);
+    }
+  });
+  
+
 
 module.exports = router;
