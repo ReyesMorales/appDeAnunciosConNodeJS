e59b7c25140d602eac3486a4e5ffd07d11f888aa
Reyes Morales Dominguez <reyesmoralesdominguez@gmail.com>
2023-02-23 20:42:57 +0000

    Añadir logica para actualizar anuncios

--- routes/createAdvert.js
+++ routes/createAdvert.js
@@ -1,8 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var Anuncio = require('../models/Anuncio');
 
-router.post('/', function(req, res, next) {
-    console.log('createadvert')
-});
+router.post('/', async (req, res, next) => {
+    try {
+  
+      const anuncioData = req.body;
+  
+      // creamos una instancia de Agente en memoria
+      const anuncio = new Anuncio (anuncioData);
+  
+      // la persistimos en la BD
+      const anuncioGuardado = await anuncio.save();
+  
+      res.json({ result: anuncioGuardado });
+  
+    } catch (error) {
+      next(error);
+    }
+  });
 
 module.exports = router;
