4f92510ec7087f675654af86a9d91d781caa3c75
Reyes Morales Dominguez <reyesmoralesdominguez@gmail.com>
2023-02-23 20:42:39 +0000

    Añadir logica para obtener un anuncio segun id

--- routes/getAdvertById.js
+++ routes/getAdvertById.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var Anuncio = require('../models/Anuncio');
 
-router.get('/:id', function(req, res, next) {
-    const id = req.params.id;
-    console.log(id);
+
+router.get('/:id', async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        console.log(id);
+        const anuncio = await Anuncio.findById(id);
+        res.json(anuncio);
+    } catch (error) {
+        next(error);
+    }
 });
 
 module.exports = router;
