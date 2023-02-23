127cb78e0d42107fb76f822d13afd1c7bb7aadc7
Reyes Morales Dominguez <reyesmoralesdominguez@gmail.com>
2023-02-23 20:42:02 +0000

    Añadir logica para obtener todos los anuncios

--- routes/getAdverts.js
+++ routes/getAdverts.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var Anuncio = require('../models/Anuncio');
 
-router.get('/', function(req, res, next) {
-    console.log('getadverts')
+router.get('/', async (req, res, next) => {
+    try {
+        const anuncios = await Anuncio.find();
+        res.json(anuncios);
+        console.log('funciona')
+    } catch (error) {
+        next(error)
+    }
 });
 
 module.exports = router;
