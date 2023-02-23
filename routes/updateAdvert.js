bbd3a4883319120f15fbedc1a9ea46fa42721bd3
Reyes Morales Dominguez <reyesmoralesdominguez@gmail.com>
2023-02-23 20:43:22 +0000

    Añadir logica para crear anuncios nuevos

--- routes/updateAdvert.js
+++ routes/updateAdvert.js
@@ -1,8 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var Anuncio = require('../models/Anuncio');
 
-router.put('/', function(req, res, next) {
-    console.log('updateadvert')
+router.put('/:id', async (req, res, next) => {
+    try {
+
+        const id = req.params.id;
+        const data = req.body;
+
+        const anuncioActualizado = await Anuncio.findByIdAndUpdate(id, data, {
+            new: true // esto hace que nos devuelva el documento actualizado
+        });
+
+        res.json({ result: anuncioActualizado });
+
+    } catch (error) {
+        next(error);
+    }
 });
+
+
 
 module.exports = router;
