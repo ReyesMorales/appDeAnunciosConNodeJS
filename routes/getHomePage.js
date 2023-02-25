dd8d0914f88efabe9bcf5d63e7f38be5b50122c0
Reyes Morales Dominguez <reyesmoralesdominguez@gmail.com>
2023-02-25 14:11:24 +0000

    Crear ruta para la home page

---
+++ routes/getHomePage.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+var Anuncio = require('../models/Anuncio');
+var Tag = require('../models/Tag');
+
+router.get('/', async (req, res, next) => {
+    const limit = parseInt(req.query.limit) || 10;
+    const skip = parseInt(req.query.skip) || 0;
+
+    const filtros = {};
+    if (req.query.tags) {
+        filtros.tags = { $in: await Tag.find({ nombre: { $in: req.query.tags.split(',') } }).select('_id') };
+    }
+    if (req.query.tipo) {
+        filtros.tipo = req.query.tipo;
+    }
+    if (req.query.precio_min) {
+        filtros.precio = { $gte: req.query.precio_min };
+    }
+    if (req.query.precio_max) {
+        filtros.precio = { ...filtros.precio, $lte: req.query.precio_max };
+    }
+    if (req.query.titulo) {
+        filtros.titulo = { $regex: req.query.titulo, $options: 'i' };
+    }
+    const anuncios = await Anuncio.find(filtros)
+    .populate('tags', 'nombre')
+    .skip(skip)
+    .limit(limit);
+
+    res.locals.anuncios = anuncios;
+    res.render('./index');
+});
+
+
+module.exports = router;
