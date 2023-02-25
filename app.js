ad06b646d7527d3f08760317edaf7487d3b81075
Reyes Morales Dominguez <reyesmoralesdominguez@gmail.com>
2023-02-25 14:10:50 +0000

    Eliminar users y añadir nombre de la app

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+require('./lib/connectMongoose');
 
 var app = express();
 
@@ -13,14 +12,17 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.locals.title = 'Nodepop';
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+                           
 
+var indexRouter = require('./routes/index');
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
